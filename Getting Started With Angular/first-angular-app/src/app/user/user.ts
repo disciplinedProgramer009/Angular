b41c10b8c3f9f4e4a17077a32196490c5500f363
sourav009 <shakaal949@gmail.com>
2025-08-16 07:53:53 +0530

    Taking input outside of component using signal

--- Getting Started With Angular/first-angular-app/src/app/user/user.ts
+++ Getting Started With Angular/first-angular-app/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, input, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,12 +10,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  @Input({required : true}) avatar!: string;
-  @Input({required : true}) name!: string;
+  // @Input({required : true}) avatar!: string;
+  // @Input({required : true}) name!: string;
 
-  get imagePath() {
-    return 'Users/' + this.avatar;
-  } 
+  avatar = input.required<string>();  // we can take input using signal's input method, we cannot assign a default value when we use required(input<string>(''))
+  name = input.required<string>();
+  // also input function of signal are readonly so we cannot reassign within the same component them as we did using the set method
+
+  imagePath = computed(() => 'Users/' + this.avatar());
+  // get imagePath() {
+  //   return 'Users/' + this.avatar;
+  // } 
 
   onSelectedUser(){
   }
