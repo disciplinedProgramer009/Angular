7980a5592c464cedf4dfdf4d1816980065f71bc5
sourav009 <shakaal949@gmail.com>
2025-08-16 06:55:37 +0530

    Signal-based state management with Angular signals

--- Getting Started With Angular/first-angular-app/src/app/user/user.ts
+++ Getting Started With Angular/first-angular-app/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,14 +10,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // data containers that hold the state (i.e. selected user and manages to update the UI)
+  imagePath = computed(() => 'Users/' + this.selectedUser().avatar); // computed property to derive the image path based on the selected user
+  // computed properties are used to derive values based on other signals or properties and are often used with signal (only gets updated when the signal on which it is dependent )
 
-  get imagePath() {
-    return 'Users/' + this.selectedUser.avatar;
-  } 
+  // get imagePath() {
+  //   return 'Users/' + this.selectedUser.avatar;
+  // } 
 
   onSelectedUser(){
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); // set method is used to set the value of the signal
   }
 }
